a716f7888c8b906ef3505d1577dc5e97ac13f786
titronius <titronius@gmail.com>
2024-03-05 14:11:17 +0300

    initial commit

---
+++ task_3/main.py
@@ -0,0 +1,22 @@
+files_names = ['1.txt', '2.txt', '3.txt']
+
+list_of_string = []
+for file_name in files_names:
+    with open(f'files/{file_name}') as f:
+        string_array = []
+        for l in f:
+            string_array.append(l.strip())
+        string_array = [file_name] + [str(len(string_array))] + string_array
+        list_of_string.append(string_array)
+
+
+list_of_string.sort(key = lambda part:part[1])
+
+final_string = ""
+for i, part in enumerate(list_of_string):
+    if i != 0:
+        final_string += '\n'
+    final_string += '\n'.join(part)
+
+with open('files/result.txt','w') as f:
+    f.write(final_string)
---
+++ tasks_1-2/main.py
@@ -0,0 +1,37 @@
+import re
+
+def is_number(s):
+    if re.match("^\d+?\.\d+?$", s) is None:
+        return s.isdigit()
+    return True
+
+cook_book = {}
+with open('files/recipes.txt') as f:
+    for i, l in enumerate(f):
+        strip_line = l.strip()
+        if i == 0 or receipt_name == False:
+            receipt_name = strip_line
+        elif strip_line == "":
+            receipt_name = False
+
+        if "|" in strip_line:
+            ingredient = strip_line.split(' | ')
+            cook_book[receipt_name].append({'ingredient_name': ingredient[0], 'quantity': int(ingredient[1]), 'measure': ingredient[2]})
+        elif is_number(strip_line):
+            pass
+        elif strip_line != "":
+            cook_book[strip_line] = []
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        for ingredient in cook_book[dish]:
+            if ingredient['ingredient_name'] in shop_list:
+                shop_list[ingredient['ingredient_name']]['quantity'] = shop_list[ingredient['ingredient_name']]['quantity'] + ingredient['quantity'] * person_count
+            else:
+                shop_list[ingredient['ingredient_name']] = {'measure': ingredient['measure'], 'quantity': ingredient['quantity'] * person_count}
+    return shop_list
+
+print(get_shop_list_by_dishes(['Обитатель утёсов', 'Фаттро', 'Татуинский закат'], 3))
+
+    # print(f.readline().strip())
